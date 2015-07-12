dfaeb0c15fea2945c531da39518f77f0b8041e77
Troy Mullaney <troym9731@gmail.com>
2015-07-12 12:58:47 -0700

    Add load functionality

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 
 var $ = require('jquery');
 var templates = require('./template');
-var db = 'http://localhost:3000/';
+var usersUrl = 'http://localhost:3000/users/';
+var tweetsUrl = 'http://localhost:3000/tweets/';
+var repliesUrl = 'http://localhost:3000/replies/';
 
 var User = {
     id: 1,
@@ -10,92 +12,83 @@ var User = {
     img: '../images/brad.png'
 };
 
-var renderTweet = function(userId, message, check) {
-    var obj = {};
+var renderTweet = function(userId, message) {
     var tweetObj = {
         userId: userId,
         message: message
-    };
-
-    console.log(tweetObj);
-
-    if (check) {
-        // tweetObj
-        // $.post(db + 'replies')
-    } else {
-        $.post(db + 'tweets', tweetObj)
-            .done(function() {
-                $.get(db + 'tweets')
-                    .done(function(tweets) {
-                    var findTweet = tweets.length - 1;
-                    var tweet = tweets[findTweet];
-                    obj.tweet = tweet;
-                    console.log(obj);
-                    }).fail(function() {
-                         console.log('fail');
-                    })
-            }).fail(function() {
-              console.log('fail');
-            });
     }
 
-    console.log(obj);
-    return obj;
+    $.post(tweetsUrl, tweetObj)
+        .done(function() {
+            loadThreads();
+        })
 }
 
-var renderThread = function(userId, message, check) {
-    check = 0;
-    var obj = renderTweet(userId, message, check);
-    console.log(obj);
-    var thread = templates.tmplThread(obj);
-    console.log(thread);
-
-    return thread;
+var renderReply = function(userId, message) {
+    var replyObj = {
+        userId: userId,
+        tweetId: tweetId,
+        message: message
+    }
 }
+
+// var renderThread = function(userId, message) {
+//     var tweet = renderTweet(userId, message);
+   
+// }
 
 // Load initial threads from Database
 var loadThreads = function() {
-    $.get(db + 'tweets')
+    $.get(tweetsUrl)
         .done(function(tweets) {
             tweets.forEach(function(tweet) {
                 var userId = tweet.userId;
                 var tweetId = tweet.id;
                 var _tweet = tweet;
-                $.get(db + 'users/' + userId)
+                $.get(usersUrl + userId)
                     .done(function(userInfo) {
                         _tweet.handle = userInfo.handle;
                         _tweet.img = userInfo.img;
+
                         var tweet = templates.tmplTweet(_tweet);
                         var obj = {
                             tweet: tweet
                         };
+
                         var thread = templates.tmplThread(obj);
                         $('#tweets').append(thread);
+
                     }).fail(function() {
                         console.log('fail');
                     })
             })
+        // Get the Replies
+
+        }).done(function() {
+            $.get(repliesUrl)
+                .done(function(replies) {
+                    replies.forEach(function(reply) {
+                        var userId = reply.userId;
+                        var tweetId = reply.tweetId;
+                        var _reply = reply;
+
+                        $.get(usersUrl + userId)
+                            .done(function(userInfo) {
+                                _reply.handle = userInfo.handle;
+                                _reply.img = userInfo.img;
+                                var reply = templates.tmplTweet(_reply);
+
+                                var $search = $('#tweet-' + tweetId);
+
+                                $search.siblings('.replies').append(reply);
+                            })
+                    })
+                }).fail(function() {
+                    console.log('fail');
+                })
         }).fail(function() {
             console.log('fail');
         });
-}
-
-var loadReplies = function(tweetId) {
-    $.get(db + 'replies')
-        .done(function(replies) {
-            replies.forEach(function(reply) {
-                if (tweetId === reply.tweetId) {
-                    var userId = reply.userid;
-                    var _reply = reply;
-                    $.get(db + 'replies/' + userId)
-                        .done(function(userInfo) {
-                            _reply.handle = userInfo.handle;
-                            _reply.img = userInfo.img;
-                            var reply = templates.tmplTweet(_reply);
-                        })
-                }
-            })
-        })
 }
 
 $(function () {
@@ -121,21 +114,12 @@ $(function () {
     $mainSection.on('click', 'button', function() {
         var $textarea = $(this).closest(composeClass).find('textarea');
         var message = $textarea.val();
-        var check = 1;
-
-        // ID counter
-
-        // var tweetId = 100;
-        // var replyId = 100;
+        
 
         if ($(this).parents().is('header')) {
-            var thread = renderThread(User.id, message, check);
-            $($tweetSection).append(thread);
-            // tweetId++;
+            renderTweet(User.id, message);
         } else {
-            var tweet = renderTweet(currentUser, message);
-            $(this).closest('.replies').append(tweet);
-            // replyId++;
+            renderReply(User.id, message);
         }
 
         $textarea.val('');
